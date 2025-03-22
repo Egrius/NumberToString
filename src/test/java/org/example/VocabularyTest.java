55c54f4b9bed1074c35e0f3f4419d51c54a855e8
Egrius <egorminsk2006@gmail.com>
2025-03-22 15:37:19 +0300

    Working Prototype 1

--- src/test/java/org/example/VocabularyTest.java
+++ src/test/java/org/example/VocabularyTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -41,20 +42,29 @@ class VocabularyTest {
         out.close();
     }
 
-
-    /*
     @Test
-    public void shouldTranslateBeforeBillion() throws IOException {
+    public void shouldTranslateNumbers() throws IOException {
         NumberTranslator numberTranslator = new NumberTranslator();
-        String fullOutputFileName = "src\\test\\java\\org\\example\\data\\numbersBeforeBillion.rba";
+        String fullOutputFileName = "src\\test\\java\\org\\example\\data\\randomNumbers.rba";
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fullOutputFileName)));
 
-        for(int i = 1000000; i < 1000000000; i++) {
-            out.println(numberTranslator.printNumber(String.valueOf(i)));
-        }
-        out.close();
-        //Лишние гигабайты памяти? Запустите этот тест
-    }
-*/
 
+        String buf = numberTranslator.printNumber(String.valueOf(1));
+        Assertions.assertEquals("один", buf);
+        out.println(buf);
+
+        buf = numberTranslator.printNumber(String.valueOf(1234));
+        Assertions.assertEquals("одна тысяча двести тридцать четыре", buf);
+        out.println(buf);
+
+        buf = numberTranslator.printNumber(String.valueOf(-150));
+        Assertions.assertEquals("минус сто пятьдесят", buf);
+        out.println(buf);
+
+        buf = numberTranslator.printNumber("11222333444");
+        Assertions.assertEquals("одиннадцать миллиардов двести двадцать два миллиона триста тридцать три тысячи четыреста сорок четыре", buf);
+        out.println(buf);
+
+        out.close();
+    }
 }
