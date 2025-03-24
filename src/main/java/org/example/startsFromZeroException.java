9e51ddae0ac8fa88531490f8306931a538bf760c
Egrius <egorminsk2006@gmail.com>
2025-03-24 23:10:35 +0300

    Decimal number part try commit

---
+++ src/main/java/org/example/startsFromZeroException.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class startsFromZeroException extends Exception{
+    public startsFromZeroException() {
+        super();
+    }
+    public startsFromZeroException(String msg) {
+        super(msg);
+    }
+}
