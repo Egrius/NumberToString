19e8314cfbb79312c49da2857a5294beb0a250eb
Egrius <egorminsk2006@gmail.com>
2025-03-29 23:54:59 +0300

    Checks for correct input

--- src/test/java/org/example/NumberTranslatorTest.java
+++ src/test/java/org/example/NumberTranslatorTest.java
@@ -8,6 +8,7 @@ class NumberTranslatorTest {
     @Test
     public void shouldTranslateBeforeThousand() {
         NumberTranslator numberTranslator = new NumberTranslator();
+
         String fullInputFileName = "src\\test\\java\\org\\example\\data\\inputFiles\\beforeThousandInput.txt";
         String fullOutputFileName = "src\\test\\java\\org\\example\\data\\outputFiles\\beforeThousandOutput.txt";
 
@@ -29,33 +30,12 @@ class NumberTranslatorTest {
     }
 
     @Test
-    public void shouldTranslateBeforeMillion() {
-        NumberTranslator numberTranslator = new NumberTranslator();
-        String fullInputFileName = "src\\test\\java\\org\\example\\data\\inputFiles\\beforeMillionInput.txt";
-        String fullOutputFileName = "src\\test\\java\\org\\example\\data\\outputFiles\\beforeMillionOutput.txt";
-
-        try {
-            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fullOutputFileName)));
-            BufferedReader reader = new BufferedReader(new FileReader(fullInputFileName));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String translatedNumber = numberTranslator.printNumber(line);
-
-                out.println(translatedNumber);
-            }
-            out.close();
-            reader.close();
-        } catch(Exception e){
-            System.err.println(e);
-        }
-    }
-
-    @Test
     public void shouldTranslateNumbers() {
        NumberTranslator numberTranslator = new NumberTranslator();
+
        String fullInputFileName = "src\\test\\java\\org\\example\\data\\inputFiles\\randomBeforeMillion.txt";
        String fullOutputFileName = "src\\test\\java\\org\\example\\data\\outputFiles\\randomBeforeMillionOutput.txt";
+
        try {
            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fullOutputFileName)));
            BufferedReader reader = new BufferedReader(new FileReader(fullInputFileName));
@@ -67,12 +47,43 @@ class NumberTranslatorTest {
 
                out.println(translatedNumber);
                out.println();
-
            }
            out.close();
            reader.close();
+
        } catch(Exception e){
            System.err.println(e);
        }
    }
+    @Test
+    public void shouldTranslateRightNumbers() {
+        NumberTranslator numberTranslator = new NumberTranslator();
+
+        String fullInputFileName = "src\\test\\java\\org\\example\\data\\inputFiles\\someNumbers.txt";
+        String fullOutputFileName = "src\\test\\java\\org\\example\\data\\outputFiles\\someNumbersOutput.txt";
+
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fullOutputFileName)));
+            BufferedReader reader = new BufferedReader(new FileReader(fullInputFileName));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                try {
+                    String translatedNumber = numberTranslator.printNumber(line);
+
+                    out.println(translatedNumber);
+                    out.println();
+                } catch (InvalidCharException e) {
+                    System.out.println("Некорректное число: " + line);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Выход за границы: " + line);
+                }
+            }
+            out.close();
+            reader.close();
+
+        } catch(Exception e){
+            System.err.println(e);
+        }
+    }
 }
